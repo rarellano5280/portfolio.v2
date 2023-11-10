7048d2dfe36fa59d9abdfa02f8f555080b699938
Robert <109295306+rarellano5280@users.noreply.github.com>
2023-11-09 17:20:29 -0700

    fixed work with me btn

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import Logo from '../assets/logo.png'
+import Logo from "../assets/logo.png";
+
+import { Link } from "react-scroll";
 
 const Header = () => {
-
-  return <header className='py-8'>
-    <div className='container mx-auto'>
-      <div className='flex justify-between items-center'>
-      <a href='#'>
-        <img className='max-w-xs'src={Logo} alt='' />
-      </a>
-      <button className='btn btn-sm'>Work with me</button>
+  return (
+    <header className="py-8">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          <a href="#">
+            <img className="max-w-xs" src={Logo} alt="" />
+          </a>
+          <Link to="contact" activeClass="active" smooth={true} spy={true}>
+            <button className="btn btn-lg">Work with me</button>
+          </Link>
+        </div>
       </div>
-    </div>
-
-  </header>
+    </header>
+  );
 };
 
 export default Header;
